758ed32795512664d2800a6bfd2aa4f781d4d726
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2023-03-10 10:42:59 +0530

    Add files via upload

---
+++ median.py
@@ -0,0 +1,16 @@
+listint=[]
+n=int(input("enter no of integers :"))
+print("enter integers :")
+for i in range(n):
+    listint.append(int(input()))
+listint.sort()
+print("list:",listint)
+length = int(n/2)
+print(length)
+if(n/length!=length):
+    median=listint[length]
+else:
+    median=(listint[length-1]+listint[length])/2
+print("median:",median)    
+    
+print("median",median)   
