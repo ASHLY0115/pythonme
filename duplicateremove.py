be2f80a4e57345f7bfefb180e9ee4a9546ecd190
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2023-03-12 15:23:17 +0530

    Update duplicateremove.py

--- duplicateremove.py
+++ duplicateremove.py
@@ -1,15 +1,20 @@
+
 numlist=[]
 print("how many numbers?")
 n=int(input())
 for i in range(n):
     num=int(input())
-    numlist.append(num)
+    numlist.append(num) 
 
 newlist=[]
 for i  in numlist:   
     if i not in newlist:
         newlist.append[i] 
-print(newlist)     
+print(newlist) 
+
+
+
+     
 
 
     
