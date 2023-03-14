b96317c42b4faf1bdb6530b8c9d1ffb567ed5a9c
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2023-03-14 10:38:52 +0530

    dict traversing

---
+++ dict3.py
@@ -0,0 +1,10 @@
+
+info={}
+info["name"] = "sandy"
+info["occupation"] = "worker"
+print(info)
+info["occupation"] = "hacker"
+print(info['name'])    
+for key in info:
+    print(key,info[key])
+  
