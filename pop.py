659af5a9e61652b2930c64e34f7926cb6c5d5b62
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2023-03-14 10:36:44 +0530

    dict...

---
+++ pop.py
@@ -0,0 +1,11 @@
+
+info={}
+info["name"] = "sandy"
+info["occupation"] = "worker"
+print(info)
+info["occupation"] = "hacker"
+print(info['name'])    
+
+print(info.pop("occupation"))
+print(info)
+    
