8b2b832d65776fef839a5aa5fce41a16ed5d0e15
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2023-03-31 09:55:09 +0530

    Create birthdayfunt.py

---
+++ birthdayfunt.py
@@ -0,0 +1,7 @@
+dict1={}
+n=int(input("enter the limit:"))
+for i in range(n):
+    name=input("enter the name")
+    dict1["name"]=input("enter the birthday")
+k=input("enter name")
+print("birthday is",dict1["k"])
